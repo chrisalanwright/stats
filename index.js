99cc31e1c72c1f238d6feeea5781be1845f7f40f
Christopher Wright <chrisalanwright@gmail.com>
2025-08-01 12:55:34 -0400

    added-comments-to-second-problem

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function getSum(numbers) {
   console.log(getSum([1, 2, 3, 4, 5])); // Output should be 15
   // I'm confident this is the method expected but could not get it to run. Would love feedback on this.
 }
-// I read about using reduce to sum the array, but I wanted to practice using a for loop.
+// I read about using reduce to sum the array, but I wanted to practice using a for loop. This method worked while testing where as the for loop did not.
 // const sum = numbers.reduce((accumulator, currentValue) => {
 //   return accumulator + currentValue;
 // }, 0);
